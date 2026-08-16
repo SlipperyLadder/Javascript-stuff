b9961b1b5198f1b065d135c80f28cf9cdd68a173
SlipperyLadder <gergo.czobor@gmail.com>
2026-08-16 20:42:52 +0100

    first commit

---
+++ Learning website/script.js
@@ -0,0 +1,11 @@
+function changeHeadingText() {
+    var headingElement = document.getElementById("latte");
+    headingElement.textContent = "did you actually hit me??";
+}
+/*
+function changeHeadingText() {
+    // Target the id of the HTML heading element
+    var headingElement = document.getElementById("myHeading");
+    // Assign new text to the HTML heading element
+    headingElement.textContent = "New Heading Text";
+}*/
